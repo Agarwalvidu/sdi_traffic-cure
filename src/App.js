8914ae2e4322b85b7ec4da32ec381c1d363bca67
Agarwalvidu <119511621+Agarwalvidu@users.noreply.github.com>
2025-01-21 02:35:45 +0530

    okay commit homepage added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import HomePage from './components/home/home';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Map from './pages/Map';
+import RewardsPage from './pages/RewardsPage';
 
 function App () {
   return (
-    <div>
-      <HomePage />
-    </div>
+    <Router>
+          <Navbar/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/reward" element={<RewardsPage />} />
+          
+        </Routes>
+        <Footer/>
+    </Router>
   );
 };
 
